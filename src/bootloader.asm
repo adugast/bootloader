e1af2287a873d850708fcc5c0f2b6f18bab1a52a
pestbuns <ant.one.dugast@gmail.com>
2017-11-26 15:53:58 +0100

    bootloader cleanup

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,17 +1,17 @@
-; Small introduction to bootloader code
-; 0x7C00 - The BIOS loads 512 bytes from this memory address - MAGIC NUMBER
-; 0xAA55 - Last two bytes of the MBR - MAGIC NUMBER
+;small introduction to bootloader code
+;0x7C00 - the BIOS loads 512 bytes from this memory address - MAGIC NUMBER
+;0xAA55 - last two bytes of the MBR - MAGIC NUMBER
 
-BITS 16             ; PC boots in real mode so only 16bit instructions
-jmp short _start
+bits 16             ;pc cpu boots in real mode so only 16bit instructions
+jmp _start
 nop
 
 string db "Printed from the bootloader", 0x0d, 0x0a, 0x00
 
 _start:
-    mov ax, 0x07C0      ; move 0x7C00 into ax
-    mov ds, ax          ; set data segment to the start
-    mov si, string      ; put string into si
+    mov ax, 0x07C0      ;move 0x7C00 into ax
+    mov ds, ax          ;set data segment to the start
+    mov si, string      ;put string into si
 
     call print_str
 
@@ -20,15 +20,15 @@ _start:
 
 
 print_str:
-    mov ah, 0x0e        ; display character, function number = 0Eh
+    mov ah, 0x0e        ;display character, function number = 0Eh
     .loop:
-        lodsb           ; load string byte to al
-        cmp al, 0x0     ; if al == 0
-        je .done        ; end function
-        int 0x10        ; else call INT 10h to print the character
-        jmp .loop       ; continue until al == 0
+        lodsb           ;load string byte to al
+        cmp al, 0x0     ;if al == 0
+        je .done        ;end function
+        int 0x10        ;else call INT 10h to print the character
+        jmp .loop       ;continue until al == 0
     .done:
         ret
 
-times 510-($-$$) db 0   ; fill the rest of MBR with 0s
-dw 0xAA55               ; The last two bytes of the MBR must contain 0xAA55
+times 510-($-$$) db 0   ;fill the rest of MBR with 0s
+dw 0xAA55               ;the last two bytes of the MBR must contain 0xAA55
